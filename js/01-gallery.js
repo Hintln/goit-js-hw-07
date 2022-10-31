a083c0cc19e5f6e0d78e1013d5efc2df61f81a22
Hintln <andrejlityprog@gmail.com>
2022-10-31 12:43:42 +0200

    +gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,30 +3,47 @@ import { galleryItems } from './gallery-items.js';
 
 
 
-console.log(basicLightbox);
+// console.log(basicLightbox);
 
-const instance = basicLightbox.create(`
-	<h1>Dynamic Content</h1>
-	<p>You can set the content of the lightbox with JS.</p>
-`)
+// const instance = basicLightbox.create(`
+// 	<h1>Dynamic Content</h1>
+// 	<p>You can set the content of the lightbox with JS.</p>
+// `)
 
-instance.show()
+// instance.show()
 
 console.log(galleryItems);
 
-const gallery = document.querySelector('.div-gallery')
+const gallery = document.querySelector('div.gallery')
 
 const createGallery =
+galleryItems.map(({ preview, original, description }) =>
+    `<div class= "gallary__item">
+        <a class = "gallery__link" href = ${original}>
+             <img
+             class="gallery__image"
+             src=${preview}    
+             data-source=${original}
+            alt=${description}
+            />
+        </a>
+    </div>`)
 
-    galleryItems.map(({ preview, original, description }) =>
+gallery.insertAdjacentHTML('beforeend', createGallery.join(''))
 
-`<div class= "gallary__item">
-<a class = "gallery__link" href = ${original}>
-<img
-class="gallery__image"
-src=${preview}    
-data-source=${original}
-alt=${description}
-/>
-</a>
-</div>`)
+
+
+gallery.addEventListener('click', openBigPicture)
+
+function openBigPicture(e) {
+    e.preventDefault()
+    if (e.taget.nodeName !== 'IMG') {
+        return
+    }
+ const instance = basicLightbox.create
+        (`img src = ${e.target.dataset.source} 
+        alt="${e.taget.alt})">`)
+
+    
+instance.show
+    }
